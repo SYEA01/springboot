bf8f1fde4c745e031318ca8fc5cceddd8151082a
taoao <taoao0101@163.com>
2023-05-24 06:30:48 +0800

    mybatisplus 基本使用

---
+++ springboot_08_ssmp/src/test/java/com/example/springboot/dao/BookDaoTestCase.java
@@ -0,0 +1,50 @@
+package com.example.springboot.dao;
+
+import com.example.springboot.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class BookDaoTestCase {
+    @Autowired
+    private BookDao bookDao;
+
+    @Test
+    void testgetById() {
+        Book book = bookDao.selectById(1);  // 根据id查询单个
+        System.out.println("book = " + book);
+    }
+
+    @Test
+    void testSave() {
+        Book book = new Book();
+        book.setType("测试数据123");
+        book.setName("测试数据123");
+        book.setDescription("测试数据123");
+        bookDao.insert(book);  //新增。需要配置id的策略
+    }
+
+    @Test
+    void testUpdate() {
+        Book book = new Book();
+        book.setId(51);
+        book.setType("测试数据324234");
+        book.setName("测试数据43423423");
+        book.setDescription("测试数据123");
+        bookDao.updateById(book);  //根据id修改
+    }
+
+    @Test
+    void testDelete() {
+        bookDao.deleteById(51);  // 根据id删除
+    }
+
+    @Test
+    void testGetAll() {
+        List<Book> books = bookDao.selectList(null);  // 查询所有
+        books.forEach(System.out::println);
+    }
+}
