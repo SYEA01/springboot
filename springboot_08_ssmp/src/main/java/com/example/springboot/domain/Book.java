97c7dba34ad272d1ad3e53d7a8d2845d77e88f36
taoao <taoao0101@163.com>
2023-05-24 05:04:51 +0800

    创建表，使用lombok简化开发实体类

--- springboot_08_ssmp/src/main/java/com/example/springboot/domain/Book.java
+++ springboot_08_ssmp/src/main/java/com/example/springboot/domain/Book.java
@@ -1,2 +1,11 @@
-package com.example.springboot.domain;public class Book {
+package com.example.springboot.domain;
+
+import lombok.Data;
+
+@Data
+public class Book {
+    private Integer id;
+    private String type;
+    private String name;
+    private String description;
 }
