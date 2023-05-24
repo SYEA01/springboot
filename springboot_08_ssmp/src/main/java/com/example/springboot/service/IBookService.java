85537223d572bb4a9345ab6adad9ed5cbe2f2e03
taoao <taoao0101@163.com>
2023-05-24 10:45:25 +0800

    controller层编写

--- springboot_08_ssmp/src/main/java/com/example/springboot/service/IBookService.java
+++ springboot_08_ssmp/src/main/java/com/example/springboot/service/IBookService.java
@@ -1,8 +1,10 @@
 package com.example.springboot.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.springboot.domain.Book;
 
 public interface IBookService extends IService<Book> {
 
+    IPage getPage(int currentPage, int pageSize);
 }
--- springboot_08_ssmp/src/main/java/com/example/springboot/service/impl/BookServiceImpl2.java
+++ springboot_08_ssmp/src/main/java/com/example/springboot/service/impl/BookServiceImpl2.java
@@ -1,11 +1,24 @@
 package com.example.springboot.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.springboot.dao.BookDao;
 import com.example.springboot.domain.Book;
 import com.example.springboot.service.IBookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookServiceImpl2 extends ServiceImpl<BookDao, Book> implements IBookService {
+
+    @Autowired
+    private BookDao bookDao;
+
+    @Override
+    public IPage getPage(int currentPage, int pageSize) {
+        IPage page = new Page(currentPage,pageSize);
+        bookDao.selectPage(page,null);
+        return page;
+    }
 }
