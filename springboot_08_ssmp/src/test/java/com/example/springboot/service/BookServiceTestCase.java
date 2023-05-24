f98530c1613d90004a777a4ad564834916aee977
taoao <taoao0101@163.com>
2023-05-24 09:12:05 +0800

    基础service层开发

---
+++ springboot_08_ssmp/src/test/java/com/example/springboot/service/BookServiceTestCase.java
@@ -0,0 +1,65 @@
+package com.example.springboot.service;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.example.springboot.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class BookServiceTestCase {
+    @Autowired
+    private BookService bookService;
+
+    @Test
+    void testGetById() {
+        Book book = bookService.getById(1);
+        System.out.println("book = " + book);
+    }
+
+    @Test
+    void testSave() {
+        Book book = new Book();
+        book.setType("测试数据123");
+        book.setName("测试数据123");
+        book.setDescription("测试数据123");
+        Boolean bl = bookService.save(book);
+        System.out.println("bl = " + bl);
+
+    }
+
+    @Test
+    void testUpdate() {
+        Book book = new Book();
+        book.setId(53);
+        book.setType("测试数据324234");
+        book.setName("测试数据43423423");
+        book.setDescription("测试数据123");
+        Boolean bl = bookService.update(book);//根据id修改
+        System.out.println("bl = " + bl);
+    }
+
+    @Test
+    void testDelete() {
+        Boolean bl = bookService.delete(53);// 根据id删除
+        System.out.println("bl = " + bl);
+    }
+
+    @Test
+    void testGetAll() {
+        List<Book> books = bookService.getAll();  // 查询所有
+        books.forEach(System.out::println);
+    }
+
+    @Test
+    void testGetPage() {
+        IPage<Book> page = bookService.getPage(2, 4);
+        System.out.println("page.getPages() = " + page.getPages());
+        System.out.println("page.getCurrent() = " + page.getCurrent());
+        System.out.println("page.getRecords() = " + page.getRecords());
+        System.out.println("page.getSize() = " + page.getSize());
+        System.out.println("page.getTotal() = " + page.getTotal());
+    }
+}
