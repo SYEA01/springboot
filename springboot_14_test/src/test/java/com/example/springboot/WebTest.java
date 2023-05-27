326f39b3e74132c47cf7f6967e9c1a1bc0c4c240
taoao <taoao0101@163.com>
2023-05-28 04:12:06 +0800

    匹配响应执行状态

--- springboot_14_test/src/test/java/com/example/springboot/WebTest.java
+++ springboot_14_test/src/test/java/com/example/springboot/WebTest.java
@@ -6,8 +6,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
 
 //webEnvironment 默认是 SpringBootTest.WebEnvironment.NONE，意思是不带web环境
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)  //随机端口
@@ -18,9 +22,21 @@ public class WebTest {
 //    @Autowired
 //    private MockMvc mockMvc;
 
+//    @Test
+//    void testWeb(@Autowired MockMvc mockMvc) throws Exception {  //方式2，将MockMvc放到参数中
+//        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books");  //模拟虚拟的http请求
+//        mockMvc.perform(builder);  //执行
+//    }
+
     @Test
-    void testWeb(@Autowired MockMvc mockMvc) throws Exception {  //方式2，将MockMvc放到参数中
-        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books");  //模拟虚拟的http请求
-        mockMvc.perform(builder);  //执行
+    void testStatus(@Autowired MockMvc mockMvc) throws Exception {
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books1");
+        ResultActions action = mockMvc.perform(builder);
+
+        //设定预期值 与真实值进行比较
+        StatusResultMatchers status = MockMvcResultMatchers.status();  //当前模拟运行的状态
+        ResultMatcher ok = status.isOk();  // 预期值  200
+
+        action.andExpect(ok);  //添加预期值到本次调用过程中进行匹配
     }
 }
