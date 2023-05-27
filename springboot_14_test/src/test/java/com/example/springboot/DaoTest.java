2604a48afb5342a68a4098cac53a83a9685dc59e
taoao <taoao0101@163.com>
2023-05-28 06:32:20 +0800

    测试用例回滚

--- springboot_14_test/src/test/java/com/example/springboot/DaoTest.java
+++ springboot_14_test/src/test/java/com/example/springboot/DaoTest.java
@@ -5,8 +5,12 @@ import com.example.springboot.service.BookService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
+@Rollback(true)   // 如果不设置@Rollback注解，或者是true，事务会自动回滚 ； 反之不会
 public class DaoTest {
     @Autowired
     private BookService bookService;
@@ -14,7 +18,7 @@ public class DaoTest {
     @Test
     void testSave(){
         Book book = new Book();
-        book.setName("springboot");
+        book.setName("springboot22222");
         book.setType("springboot");
         book.setDescription("springboot");
         bookService.save(book);
