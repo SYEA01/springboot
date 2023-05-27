0d825ac350e81b62d82f99d3cc64a31db7c46530
taoao <taoao0101@163.com>
2023-05-28 03:49:26 +0800

    发送虚拟请求

--- springboot_14_test/src/test/java/com/example/springboot/WebTest.java
+++ springboot_14_test/src/test/java/com/example/springboot/WebTest.java
@@ -1,13 +1,26 @@
 package com.example.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 //webEnvironment 默认是 SpringBootTest.WebEnvironment.NONE，意思是不带web环境
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)  //随机端口
+@AutoConfigureMockMvc  //开启虚拟MVC的调用
 public class WebTest {
-    @Test
-    void test(){
 
+    // 方式1，注入MockMvc
+//    @Autowired
+//    private MockMvc mockMvc;
+
+    @Test
+    void testWeb(@Autowired MockMvc mockMvc) throws Exception {  //方式2，将MockMvc放到参数中
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books");  //模拟虚拟的http请求
+        mockMvc.perform(builder);  //执行
     }
 }
