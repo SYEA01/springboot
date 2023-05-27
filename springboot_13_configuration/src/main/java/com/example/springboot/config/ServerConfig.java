7d30f4369b8a0429e1ca6fb4c4ae492361ed68e9
taoao <taoao0101@163.com>
2023-05-27 11:24:17 +0800

    视频中提到的校验

--- springboot_13_configuration/src/main/java/com/example/springboot/config/ServerConfig.java
+++ springboot_13_configuration/src/main/java/com/example/springboot/config/ServerConfig.java
@@ -7,15 +7,21 @@ import org.springframework.boot.convert.DurationUnit;
 import org.springframework.stereotype.Component;
 import org.springframework.util.unit.DataSize;
 import org.springframework.util.unit.DataUnit;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
 //@Component
 @Data
 @ConfigurationProperties(prefix = "servers")
+@Validated  // 开启对当前bean的属性注入校验
 public class ServerConfig {
     private String ipAddress;
+    @Max(value = 8888,message = "最大值不能超过8888")
+//    @Min(value = 202,message = "最小值不能低于202")
     private int port;
     private long timeout;
 
