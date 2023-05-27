ba0bbe447b02c5185f05de27ef13a57a020cac60
taoao <taoao0101@163.com>
2023-05-27 10:43:44 +0800

    常用计量单位绑定

--- springboot_13_configuration/src/main/java/com/example/springboot/config/ServerConfig.java
+++ springboot_13_configuration/src/main/java/com/example/springboot/config/ServerConfig.java
@@ -2,7 +2,14 @@ package com.example.springboot.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.convert.DataSizeUnit;
+import org.springframework.boot.convert.DurationUnit;
 import org.springframework.stereotype.Component;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 //@Component
 @Data
@@ -11,4 +18,10 @@ public class ServerConfig {
     private String ipAddress;
     private int port;
     private long timeout;
+
+    @DurationUnit(ChronoUnit.HOURS)  // 代表以小时为单位
+    private Duration serverTimeOut;  // PT3H
+
+    @DataSizeUnit(DataUnit.BYTES)  // 数据存储单位  代表byte
+    private DataSize dataSize;  // 10B
 }
