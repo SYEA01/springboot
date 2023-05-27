9613978732402b127aea73e643ef4177cc018355
taoao <taoao0101@163.com>
2023-05-27 08:43:02 +0800

    配置关闭热部署

--- springboot_12_hot_deploy/src/main/java/com/example/springboot/SSMPApplication.java
+++ springboot_12_hot_deploy/src/main/java/com/example/springboot/SSMPApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SSMPApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.devtools.restart.enabled","false");  // 以更高的优先级关闭热部署。这个优先级比application.properties高
 		SpringApplication.run(SSMPApplication.class, args);
 	}
 
