7239ac8034ba285a6986653cc9b7165c3b2b8caa
taoao <taoao0101@163.com>
2023-05-28 08:29:46 +0800

    springboot内置持久化解决方案--JdbcTemplate

--- springboot_15_sql/src/test/java/com/example/springboot/Springboot15SqlApplicationTests.java
+++ springboot_15_sql/src/test/java/com/example/springboot/Springboot15SqlApplicationTests.java
@@ -1,20 +1,55 @@
 package com.example.springboot;
 
+import com.example.springboot.dao.BookDao;
 import com.example.springboot.domain.Book;
-import com.example.springboot.domain.BookDao;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class Springboot15SqlApplicationTests {
 
-    @Autowired
-    private BookDao bookDao;
+//    @Autowired
+//    private BookDao bookDao;
 
     @Test
     void contextLoads() {
-        bookDao.selectById(1);
+//        bookDao.selectById(1);
     }
 
+
+    @Test
+    void testJdbcTemplate(@Autowired JdbcTemplate jdbcTemplate) {
+        String sql = "select * from tbl_book";
+//        List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
+//        maps.forEach(System.out::println);
+        RowMapper<Book> rm = new RowMapper<Book>() {
+            @Override
+            public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Book temp = new Book();
+                temp.setId(rs.getInt("id"));
+                temp.setName(rs.getString("name"));
+                temp.setType(rs.getString("type"));
+                temp.setDescription(rs.getString("description"));
+
+                return temp;
+            }
+        };
+        List<Book> list = jdbcTemplate.query(sql, rm);
+        list.forEach(System.out::println);
+    }
+    @Test
+    void testJdbcTemplateSave(@Autowired JdbcTemplate jdbcTemplate) {
+        String sql = "insert into tbl_book values(null,'springboot1','springboot2','springboot2')";
+        jdbcTemplate.update(sql);
+    }
 }
