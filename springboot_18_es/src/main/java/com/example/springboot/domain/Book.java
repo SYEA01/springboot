998d3b6eeeda5dd8964c3f98bdeb005efb77b3db
taoao <taoao0101@163.com>
2023-05-29 15:36:17 +0800

    准备

---
+++ springboot_18_es/src/main/java/com/example/springboot/domain/Book.java
@@ -0,0 +1,11 @@
+package com.example.springboot.domain;
+
+import lombok.Data;
+
+@Data
+public class Book {
+    private Integer id;
+    private String name;
+    private String type;
+    private String description;
+}
