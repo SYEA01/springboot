33b85879bdfcbacf9172e387c939829a7d11053b
taoao <taoao0101@163.com>
2023-05-29 16:40:06 +0800

    连接es客户端

--- springboot_18_es/src/test/java/com/example/springboot/Springboot18EsApplicationTests.java
+++ springboot_18_es/src/test/java/com/example/springboot/Springboot18EsApplicationTests.java
@@ -1,18 +1,46 @@
 package com.example.springboot;
 
-import com.example.springboot.dao.BookDao;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class Springboot18EsApplicationTests {
+    //    @Autowired
+//    private ElasticsearchRestTemplate template;   // 不推荐使用，老版本
 
-    @Autowired
-    private BookDao bookDao;
-    @Test
-    void contextLoads() {
-        bookDao.selectById(1);
+
+    private RestHighLevelClient client;
+
+    @BeforeEach
+    void setUp() {
+        //连接
+        HttpHost host = HttpHost.create("http://192.168.0.111:9200");
+        RestClientBuilder builder = RestClient.builder(host);
+        client = new RestHighLevelClient(builder);
     }
 
+    @AfterEach
+    void tearDown() throws IOException {
+        //关闭
+        client.close();
+    }
+
+    /**
+     * 创建索引
+     */
+    @Test
+    void testCreateIndex() throws IOException {
+        CreateIndexRequest request = new CreateIndexRequest("books");
+        client.indices().create(request, RequestOptions.DEFAULT);
+    }
 }
