e95251fdeb5d7c96421dea38afe21b3ee21d2f5f
taoao <taoao0101@163.com>
2023-05-31 01:51:08 +0800

    准备

---
+++ springboot_21_j2cache/src/main/java/com/example/springboot/controller/SMSCodeController.java
@@ -0,0 +1,25 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.domain.SMSCode;
+import com.example.springboot.service.SMSCodeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/sms")
+public class SMSCodeController {
+
+    @Autowired
+    private SMSCodeService service;
+
+    @GetMapping
+    public String getCode(String tele){
+        String code = service.sendCodeToSMS(tele);
+        return code;
+    }
+
+    @PostMapping
+    public boolean checkCode(@RequestBody SMSCode smsCode){
+        return service.checkCode(smsCode);
+    }
+}
