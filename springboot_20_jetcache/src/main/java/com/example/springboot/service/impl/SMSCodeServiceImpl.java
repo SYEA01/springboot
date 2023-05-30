224895a0147adbfcc519332c7fcb43ace117ff27
taoao <taoao0101@163.com>
2023-05-30 12:03:51 +0800

    整合jetcache加上本地缓存方案

--- springboot_20_jetcache/src/main/java/com/example/springboot/service/impl/SMSCodeServiceImpl.java
+++ springboot_20_jetcache/src/main/java/com/example/springboot/service/impl/SMSCodeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.springboot.service.impl;
 
 import com.alicp.jetcache.Cache;
+import com.alicp.jetcache.anno.CacheType;
 import com.alicp.jetcache.anno.CreateCache;
 import com.example.springboot.domain.SMSCode;
 import com.example.springboot.service.SMSCodeService;
@@ -19,12 +20,14 @@ public class SMSCodeServiceImpl implements SMSCodeService {
     // 自定义缓存空间  import com.alicp.jetcache.Cache;
 
     /**
-     * @CreateCache(name = "jetCache",expire = 3600,timeUnit = TimeUnit.SECONDS)：告诉jetCache 这是一个用来缓存的对象。
+     * @CreateCache(name = "jetCache",expire = 3600,timeUnit = TimeUnit.SECONDS)：远程的方案。告诉jetCache 这是一个用来缓存的对象。
      * 这个name等同于@Cacheable注解的value；
      * expire 指定过期时间；
      * timeUnit = TimeUnit.SECONDS 单位默认是秒，这就是默认值
+     * cacheType   有三种选择：只用远程、只用本地、两者都用.默认只用远程
+     * area:  就是remote或者local下面的属性名，默认时default
      */
-    @CreateCache(name = "jetCache",expire = 3600,timeUnit = TimeUnit.SECONDS)
+    @CreateCache(area="default",name = "jetCache",expire = 3600,timeUnit = TimeUnit.SECONDS,cacheType = CacheType.LOCAL)
     private Cache<String,String> jetCache;
     @Override
     public String sendCodeToSMS(String tele) {
@@ -39,7 +42,6 @@ public class SMSCodeServiceImpl implements SMSCodeService {
         String code = smsCode.getCode();
         String queryCode = jetCache.get(smsCode.getTele());  // 从缓存中取出
         return code.equals(queryCode);
-//        return true;
     }
 
 
