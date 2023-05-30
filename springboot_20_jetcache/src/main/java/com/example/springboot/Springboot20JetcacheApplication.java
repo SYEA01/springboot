79ab627313356849e475096254e73e6d9e410e35
taoao <taoao0101@163.com>
2023-05-30 11:46:02 +0800

    使用jetcache远程缓存方案

--- springboot_20_jetcache/src/main/java/com/example/springboot/Springboot20JetcacheApplication.java
+++ springboot_20_jetcache/src/main/java/com/example/springboot/Springboot20JetcacheApplication.java
@@ -1,9 +1,11 @@
 package com.example.springboot;
 
+import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableCreateCacheAnnotation  // jetCache的注解。是jetcache启用缓存的主开关   启用使用注解方式创建缓存
 public class Springboot20JetcacheApplication {
 
     public static void main(String[] args) {
--- springboot_20_jetcache/src/main/java/com/example/springboot/service/impl/SMSCodeServiceImpl.java
+++ springboot_20_jetcache/src/main/java/com/example/springboot/service/impl/SMSCodeServiceImpl.java
@@ -1,10 +1,14 @@
 package com.example.springboot.service.impl;
 
+import com.alicp.jetcache.Cache;
+import com.alicp.jetcache.anno.CreateCache;
 import com.example.springboot.domain.SMSCode;
 import com.example.springboot.service.SMSCodeService;
 import com.example.springboot.utils.CodeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class SMSCodeServiceImpl implements SMSCodeService {
@@ -12,9 +16,20 @@ public class SMSCodeServiceImpl implements SMSCodeService {
     @Autowired
     private CodeUtils codeUtils;
 
+    // 自定义缓存空间  import com.alicp.jetcache.Cache;
+
+    /**
+     * @CreateCache(name = "jetCache",expire = 3600,timeUnit = TimeUnit.SECONDS)：告诉jetCache 这是一个用来缓存的对象。
+     * 这个name等同于@Cacheable注解的value；
+     * expire 指定过期时间；
+     * timeUnit = TimeUnit.SECONDS 单位默认是秒，这就是默认值
+     */
+    @CreateCache(name = "jetCache",expire = 3600,timeUnit = TimeUnit.SECONDS)
+    private Cache<String,String> jetCache;
     @Override
     public String sendCodeToSMS(String tele) {
         String code = codeUtils.generator(tele);
+        jetCache.put(tele,code);  // 放入缓存
         return code;
     }
 
@@ -22,9 +37,10 @@ public class SMSCodeServiceImpl implements SMSCodeService {
     public boolean checkCode(SMSCode smsCode) {
         //取出内存中的与传进来的比对
         String code = smsCode.getCode();
-        return true;
+        String queryCode = jetCache.get(smsCode.getTele());  // 从缓存中取出
+        return code.equals(queryCode);
+//        return true;
     }
-
 
 
 }
