6c9dc7a097be14dc00eef0e1993d3a81708d8928
taoao <taoao0101@163.com>
2023-05-31 02:08:47 +0800

    整合j2cache

--- springboot_21_j2cache/src/main/java/com/example/springboot/service/impl/SMSCodeServiceImpl.java
+++ springboot_21_j2cache/src/main/java/com/example/springboot/service/impl/SMSCodeServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.springboot.service.impl;
 import com.example.springboot.domain.SMSCode;
 import com.example.springboot.service.SMSCodeService;
 import com.example.springboot.utils.CodeUtils;
+import net.oschina.j2cache.CacheChannel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,9 +14,13 @@ public class SMSCodeServiceImpl implements SMSCodeService {
     @Autowired
     private CodeUtils codeUtils;
 
+    @Autowired
+    private CacheChannel cacheChannel;  //缓存对象
+
     @Override
     public String sendCodeToSMS(String tele) {
         String code = codeUtils.generator(tele);
+        cacheChannel.set("sms", tele, code);  //放入缓存
         return code;
     }
 
@@ -23,7 +28,8 @@ public class SMSCodeServiceImpl implements SMSCodeService {
     public boolean checkCode(SMSCode smsCode) {
         //取出内存中的与传进来的比对
         String code = smsCode.getCode();
-        return code.equals("");
+        String queryCode = cacheChannel.get("sms",smsCode.getTele()).asString();  //从缓存取出
+        return code.equals(queryCode);
     }
 
 
