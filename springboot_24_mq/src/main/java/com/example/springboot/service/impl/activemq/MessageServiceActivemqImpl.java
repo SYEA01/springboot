d7ac37404dd09303e41582afe3200910d72aa36d
taoao <taoao0101@163.com>
2023-05-31 11:51:31 +0800

    整合RabbitMQ---直连交换机direct

--- springboot_24_mq/src/main/java/com/example/springboot/service/impl/activemq/MessageServiceActivemqImpl.java
+++ springboot_24_mq/src/main/java/com/example/springboot/service/impl/activemq/MessageServiceActivemqImpl.java
@@ -1,26 +1,26 @@
-package com.example.springboot.service.impl.activemq;
-
-import com.example.springboot.service.MessageService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jms.core.JmsMessagingTemplate;
-import org.springframework.stereotype.Service;
-
-@Service
-public class MessageServiceActivemqImpl implements MessageService {
-
-    @Autowired
-    private JmsMessagingTemplate messagingTemplate;
-
-    @Override
-    public void sendMessage(String id) {
-        System.out.println("id:"+id+"放到队列中");
-        messagingTemplate.convertAndSend("order.queue.id",id);  // 放到消息队列中
-    }
-
-    @Override
-    public String doMessage() {
-        String id = messagingTemplate.receiveAndConvert("order.queue.id",String.class);  //从消息队列中取消息（一般不用这种方法接收消息，都是使用注解@JmsListener自动监听消息）
-        System.out.println("id:" + id + "已经取出");
-        return id;
-    }
-}
+//package com.example.springboot.service.impl.activemq;
+//
+//import com.example.springboot.service.MessageService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.jms.core.JmsMessagingTemplate;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class MessageServiceActivemqImpl implements MessageService {
+//
+//    @Autowired
+//    private JmsMessagingTemplate messagingTemplate;
+//
+//    @Override
+//    public void sendMessage(String id) {
+//        System.out.println("id:"+id+"放到队列中");
+//        messagingTemplate.convertAndSend("order.queue.id",id);  // 放到消息队列中
+//    }
+//
+//    @Override
+//    public String doMessage() {
+//        String id = messagingTemplate.receiveAndConvert("order.queue.id",String.class);  //从消息队列中取消息（一般不用这种方法接收消息，都是使用注解@JmsListener自动监听消息）
+//        System.out.println("id:" + id + "已经取出");
+//        return id;
+//    }
+//}
